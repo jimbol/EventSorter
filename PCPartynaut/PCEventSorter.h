4f5dd96b3b8490ee00bad2accbf01a702dd59c70
Jim Hall <blueeyeguy.jim@gmail.com>
2012-05-16 15:20:19 -0500

    Update PCPartynaut/PCEventSorter.h

--- PCPartynaut/PCEventSorter.h
+++ PCPartynaut/PCEventSorter.h
@@ -2,7 +2,7 @@
 //  PCEventSorter.h
 //  PCPartynaut
 //
-//  Created by Chris Mack on 4/28/12.
+//  Created by Jim Hall on 4/28/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
